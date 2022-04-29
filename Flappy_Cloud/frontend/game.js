68bed6bf0395ae8e0da91da1c45b041b62b0e532
Fanny <fannysarasoderlund@gmail.com>
2022-04-29 14:54:31 +0200

    Start page for login, statistics

--- Flappy_Cloud/frontend/game.js
+++ Flappy_Cloud/frontend/game.js
@@ -1,29 +1,17 @@
-// Background scrolling speed
 let move_speed = 3;
+let gravity = 0.35;
 
-// Gravity constant value
-let gravity = 0.4;
-
-// Getting reference to the bird element
 let bird = document.querySelector('.bird');
-
-// Getting bird element properties
 let bird_props = bird.getBoundingClientRect();
-
 let background = document.querySelector('body').getBoundingClientRect();
-
-// Getting reference to the score element
 let score_val = document.querySelector('.score_val');
 let message = document.querySelector('.message');
 let score_title = document.querySelector('.score_title');
 
-// Setting initial game state to start
 let game_state = 'Start';
 
-// Add an eventlistener for key presses
 document.addEventListener('click', (e) => {
 
-  // Start the game if enter key is pressed
   if (game_state !== 'Play') {
 
     console.log(game_state);
@@ -45,41 +33,34 @@ document.addEventListener('click', (e) => {
 function play() {
   function move() {
 
-    // Detect if game has ended
     if (game_state !== 'Play') return;
 
-    // Getting reference to all the pipe elements
     let pipe_sprite = document.querySelectorAll('.pipe_sprite');
     pipe_sprite.forEach((element) => {
 
       let pipe_sprite_props = element.getBoundingClientRect();
       bird_props = bird.getBoundingClientRect();
 
-      // Delete the pipes if they have moved out
-      // of the screen hence saving memory
       if (pipe_sprite_props.right <= 0) {
         element.remove();
+
       } else {
-        // Collision detection with bird and pipes
+
         if (
-          bird_props.left + 50 < pipe_sprite_props.left +
+          bird_props.left + 70 < pipe_sprite_props.left +
           pipe_sprite_props.width &&
-          bird_props.left - 50 +
+          bird_props.left - 90 +
           bird_props.width > pipe_sprite_props.left &&
-          bird_props.top + 20 < pipe_sprite_props.top +
+          bird_props.top + 50 < pipe_sprite_props.top +
           pipe_sprite_props.height &&
-          bird_props.top - 30 +
+          bird_props.top - 40 +
           bird_props.height > pipe_sprite_props.top
         ) {
 
-          // Change game state and end the game
-          // if collision occurs
+          gameOver()
 
-          game_state = 'End';
-          location.reload();
         } else {
-          // Increase the score if player
-          // has the successfully dodged the
+
           if (
             pipe_sprite_props.right < bird_props.left + 50 &&
             pipe_sprite_props.right +
@@ -87,6 +68,10 @@ function play() {
             element.increase_score == '1'
           ) {
             score_val.innerHTML = +score_val.innerHTML + 1;
+
+            if (score_val % 3 === 0) {
+              document.body.style.boxShadow = 'inset 400px 400px 400px 400px rgba(0, 0, 0, .5)';
+            }
           }
           element.style.left =
             pipe_sprite_props.left - move_speed + 'px';
@@ -102,18 +87,19 @@ function play() {
 
   function apply_gravity() {
     bird_dy = bird_dy + gravity;
-    document.addEventListener('click', () => {
-      bird_dy = -7.6;
 
-    });
+    document.addEventListener('click', (e) => {
+      bird_dy = -7;
 
-    // Collision detection with bird and
-    // window top and bottom
+    })
+    document.addEventListener('keydown', (e) => {
+      if (e.code === 'Space') bird_dy = -7;
+
+    })
 
     if (bird_props.top <= 0 ||
       bird_props.bottom >= background.bottom) {
-      game_state = 'End';
-      location.reload();
+      gameOver();
     }
 
     bird.style.top = bird_props.top + bird_dy + 'px';
@@ -125,37 +111,46 @@ function play() {
 
   let pipe_seperation = 0;
 
-  // Constant value for the gap between two pipes
-  let pipe_gap = 25;
+  let pipe_gap = 20;
+
   function create_pipe() {
 
-
-    // Create another set of pipes
-    // if distance between two pipe has exceeded
-    // a predefined value
-    if (pipe_seperation > 115) {
+    if (pipe_seperation > 120) {
       pipe_seperation = 0
 
-      // Calculate random position of pipes on y axis
       let pipe_posi = Math.floor(Math.random() * 43) + 8;
-      let pipe_sprite_inv = document.createElement('div');
+      let pipe_sprite_inv = document.createElement('img');
+      pipe_sprite_inv.src = './resources/cloud.png';
       pipe_sprite_inv.className = 'pipe_sprite';
-      pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
+      pipe_sprite_inv.style.top = pipe_posi - 50 + 'vh';
       pipe_sprite_inv.style.left = '100vw';
 
-      // Append the created pipe element in DOM
       document.body.appendChild(pipe_sprite_inv);
-      let pipe_sprite = document.createElement('div');
+      let pipe_sprite = document.createElement('img');
+      pipe_sprite.src = './resources/cloud.png';
       pipe_sprite.className = 'pipe_sprite';
       pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
       pipe_sprite.style.left = '100vw';
       pipe_sprite.increase_score = '1';
 
-      // Append the created pipe element in DOM
       document.body.appendChild(pipe_sprite);
     }
     pipe_seperation++;
     requestAnimationFrame(create_pipe);
   }
-  requestAnimationFrame(create_pipe);
+  requestAnimationFrame(create_pipe)
+}
+
+
+function gameOver() {
+  document.querySelectorAll('.pipe_sprite').forEach((e) => {
+    e.remove();
+  });
+
+  message.innerHTML = 'You hit something..<br><p>Your journey is over</p>';
+
+  setInterval(() => {
+    location.replace('http://127.0.0.1:5500/Flappy_Cloud/frontend/index.html');
+  }, 2000);
+
 }
---
+++ Flappy_Cloud/frontend/start.js
@@ -0,0 +1,4 @@
+
+function playGame() {
+  location.replace('http://127.0.0.1:5500/Flappy_Cloud/frontend/game.html');
+} 
