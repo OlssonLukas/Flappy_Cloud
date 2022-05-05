c1d7ec8cacecdfe02f3df66cb57d7b329c9be0c2
Fanny <fannysarasoderlund@gmail.com>
2022-05-05 18:50:59 +0200

    Frontend sign in and up, stay signed in through game sessions

--- Flappy_Cloud/frontend/start.js
+++ Flappy_Cloud/frontend/start.js
@@ -1,4 +1,5 @@
-
+let signedin = false;
+let user = '';
 
 document.body.addEventListener('click', (e) => {
 
@@ -22,25 +23,90 @@ document.body.addEventListener('click', (e) => {
   }
   else if (button?.className === 'signin-button') {
     document.querySelector('.dropdown-content-up').style.display = 'none';
-    console.log('CLICKED');
     if (document.querySelector('.dropdown-content-in').style.display === 'block') {
-      console.log('its block');
       document.querySelector('.dropdown-content-in').style.display = 'none';
     } else {
-      console.log('its hidden');
       document.querySelector('.dropdown-content-in').style.display = 'block';
     }
   }
 
   else if (button?.className === 'signup-button') {
     document.querySelector('.dropdown-content-in').style.display = 'none';
-    console.log('CLICKED');
     if (document.querySelector('.dropdown-content-up').style.display === 'block') {
-      console.log('its block');
       document.querySelector('.dropdown-content-up').style.display = 'none';
     } else {
-      console.log('its hidden');
       document.querySelector('.dropdown-content-up').style.display = 'block';
     }
   }
 });
+
+document.body.addEventListener('click', (e) => {
+
+  let button = e.target.closest('input');
+
+  if (button?.id === 'login-form-submit') {
+    e.preventDefault();
+    const username = document.querySelector('#login-form').username.value;
+    const password = document.querySelector('#login-form').password.value;
+
+    //make call to see if login is correct
+    if (username === 'user' && password === 'pass') {
+
+      user = username.toUpperCase();
+
+      changeHomeScreen(user);
+      signedin = true;
+    }
+
+  } else if (button?.id === 'signup-form-submit') {
+    e.preventDefault();
+    const username = document.querySelector('#signup-form').username.value;
+    const password = document.querySelector('#signup-form').password.value;
+
+    //make call to create user
+    if (username === 'user' && password === 'pass') {
+      user = username.toUpperCase();
+      changeHomeScreen(user);
+      signedin = true;
+    }
+
+  }
+});
+
+
+function changeHomeScreen(username) {
+  document.querySelector('.content').innerHTML = ` <div class="menu">
+      <h1>Flappy Cloud</h1>
+      <div class="signin">
+          <p>Captain ${username}</p>
+          <button>Sign out</button>
+      </div>
+    </div>
+
+    <div class="wrapper">
+
+      <div class="statistics">
+        <div class="leaderboard">
+          <h2>Your top scores</h2>
+          <ul>
+            <li>${username}: 56</li>
+            <li>${username}: 32</li>
+            <li>${username}: 23</li>
+            <li>${username}: 18</li>
+            <li>${username}: 8</li>
+            <li>${username}: 5</li>
+            <li>${username}: 2</li>
+          </ul>
+        </div>
+        <div class="totalFlaps">
+          <h2>YOUR LONGEST PLAYTIME: </h2>
+          <h2> 600 s</h2>
+        </div>
+      </div>
+      <div class="start-game">
+        <img src="./resources/birdScout.gif" alt="">
+        <button class="play-button">Start flying</button>
+      </div>
+    </div>
+`;
+}
